22475bed3c36e8426e82baef41d0d53cdafe7ed5
Rodrigo Diniz <santos.rodrigodiniz@gmail.com>
2018-11-27 14:40:50 -0200

    Validações Requisições Ajax

--- notas-fiscais/src/br/com/notasfiscais/beans/ProdutoBean.java
+++ notas-fiscais/src/br/com/notasfiscais/beans/ProdutoBean.java
@@ -1,30 +1,63 @@
 package br.com.notasfiscais.beans;
 
-
 import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ViewScoped;
 
 import br.com.notasfiscais.dao.DAO;
 import br.com.notasfiscais.modelo.Produto;
 
-@RequestScoped
+@ViewScoped
 @ManagedBean(name = "produtoBean")
 public class ProdutoBean {
 
 	private Produto produto;
-	
-	private List<Produto>produtos;
-	
+
+	private List<Produto> produtos;
+
 	private Double total;
-	
-	
+
 	@PostConstruct
-	public void inicializarObjetos(){
-	    this.produto = new Produto();
-	    this.total = 0.0;
+	public void inicializarObjetos() {
+		this.produto = new Produto();
+		this.total = 0.0;
+	}
+
+	public void grava() {
+		DAO<Produto> dao = new DAO<Produto>(Produto.class);
+		if (this.produto.getId() == null) {
+			
+			dao.adiciona(this.produto);
+		}else{
+			
+			dao.atualiza(this.produto);
+		}
+		this.produto = new Produto();
+		//para atualiza a lista após a gravação de um novo registro
+		this.produtos = dao.listaTodos();
+	}
+	
+	public void remove(Produto produto) {
+		DAO<Produto>dao = new DAO<Produto>(Produto.class);
+		dao.remove(produto);
+		//para atualiza a lista após a remoção de um registro
+		this.produtos = dao.listaTodos();
+	}
+	
+	public void cancelarEdicao() {
+		this.produto = new Produto();
+	}
+
+	public List<Produto> getProdutos() {
+		if (this.produtos == null) {
+			System.out.println("Carregando lista de Produtos...");
+			this.produtos = new DAO<Produto>(Produto.class).listaTodos();
+		}
+		getTotal();
+		return produtos;
 	}
 
 	public Produto getProduto() {
@@ -35,29 +68,23 @@ public class ProdutoBean {
 		this.produto = produto;
 	}
 
-	public void grava() {
-		DAO<Produto> dao = new DAO<Produto>(Produto.class);
-		dao.adiciona(produto);
-		this.produto = new Produto();
-		this.produtos = dao.listaTodos();
-	}
-
-	public List<Produto> getProdutos() {
-		if (produtos == null) {
-			System.out.println("Carregando lista de Produtos...");
-			produtos = new DAO<Produto>(Produto.class).listaTodos();
-		}
-		getTotal();
-		return produtos;
-	}
-
 	public Double getTotal() {
 		double somaVlr = 0.0;
 		for (Produto produto : produtos) {
 			somaVlr = somaVlr + produto.getPreco();
 		}
-		return this.total = somaVlr;
+		this.total = somaVlr;
+		return total;
 	}
+
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+
 	
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
 
 }
